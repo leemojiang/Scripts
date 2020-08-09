26e2964c893c6c1afa29912b28c39e1f213cb498
Your Name <you@example.com>
2020-08-09 13:57:07 +0800

    加入了第二种更适合鼠标的一阶控制方式

--- RotationalBundle.cs
+++ RotationalBundle.cs
@@ -29,11 +29,17 @@ public class RotationalBundle:MonoBehaviour{
     [SerializeField]float dirX,dirY,dirZ; //direction for x,y,z used for deacceleration
 
     //current rotation speed
-    [SerializeField] Vector3 targetEulerAngle;
+    [SerializeField] Vector3 targetEulerAngle; //every frame updated once //indicate where the rotation should be in one frame constrained by the acc and maxspeed
+
     
+    void Start(){
+        initRotation=transform.localRotation;
+        targetEulerAngle=transform.localEulerAngles;
+    }
+
     void Update(){
         continousRotation();
-        manualRotation();
+        manualRotation2();
     }
 
     void continousRotation(){
@@ -41,7 +47,8 @@ public class RotationalBundle:MonoBehaviour{
     }
 
 
-    void manualRotation(){
+    void manualRotation()
+    {
         //good for keyboard rotation but bad for mouse
 
         inputX= Input.GetAxis(setInputToRoll.ToString());
@@ -52,6 +59,7 @@ public class RotationalBundle:MonoBehaviour{
         {
             curX=Mathf.MoveTowards(curX,setMaxSpeed.x,setAcceleration.x*Time.deltaTime);
             dirX=Mathf.Sign(inputX);
+            isRotating=true;
         }else{
             curX=Mathf.MoveTowards(curX,0,setDeAcceleration.x*Time.deltaTime);         
         }
@@ -69,10 +77,66 @@ public class RotationalBundle:MonoBehaviour{
 
         targetEulerAngle.x= Mathf.Clamp(targetEulerAngle.x, setMinRotation.x, setMaxRotation.x);
 
-        transform.localEulerAngles=targetEulerAngle;        
+        transform.localEulerAngles=targetEulerAngle; 
+        // rotateToTarget();    
     }
-
     
+    [Range(1,199)]
+    public float sensitivX;
+    void manualRotation2(){
+        inputX= Input.GetAxis(setInputToRoll.ToString());
+        inputY=Input.GetAxis(setInputToYaw.ToString());
+        inputZ=Input.GetAxis(setInputToPitch.ToString());
+        if (inputX!=0)
+        {
+            curX=Mathf.MoveTowards(curX,setMaxSpeed.x,setAcceleration.x*Time.deltaTime);
+            dirX=Mathf.Sign(inputX);
+            isRotating=true;
+        }else{
+            curX=Mathf.MoveTowards(curX,0,setDeAcceleration.x*Time.deltaTime);         
+        }
+
+
+        float deltaX=Mathf.Clamp(Mathf.Abs(inputX*sensitivX),0,curX*Time.deltaTime);
+        // deltaX=Mathf.Clamp(Mathf.Abs(inputX*sensitivX),0,setMaxSpeed.x*Time.deltaTime);
+
+        targetEulerAngle.x=targetEulerAngle.x+deltaX*dirX;
+        
+        //enable full cycle
+        if (targetEulerAngle.x <=-180)
+        {
+            targetEulerAngle.x+=360;
+        }
+        if (targetEulerAngle.x >= 180)
+        {
+            targetEulerAngle.x-=360;
+        }
+
+        targetEulerAngle.x= Mathf.Clamp(targetEulerAngle.x, setMinRotation.x, setMaxRotation.x);
+
+        transform.localEulerAngles=targetEulerAngle; 
+    }
+    bool isRotating=false;
+    private Quaternion initRotation;
+    void rotateToTarget_Transform(){
+        if (!isRotating)
+        {             
+            return;
+        }
+
+        float targetX,targetY,targetZ;
+
+        targetX=Mathf.DeltaAngle(transform.localEulerAngles.x,targetEulerAngle.x);
+        targetY=Mathf.DeltaAngle(transform.localEulerAngles.y,targetEulerAngle.y);
+        targetZ=Mathf.DeltaAngle(transform.localEulerAngles.z,targetEulerAngle.z);
+
+        if (Mathf.Abs (targetX) < 0.01f && Mathf.Abs (targetY) < 0.01f && Mathf.Abs (targetZ) < 0.01f) {
+			isRotating = false;
+		}
+
+        Quaternion rot=initRotation * Quaternion.Euler(targetEulerAngle.x,targetEulerAngle.y,targetEulerAngle.z);
+        transform.localRotation=Quaternion.Slerp(transform.localRotation,rot,Time.deltaTime);
+    }
 
 
 
