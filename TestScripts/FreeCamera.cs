7a187e4ac68c0a8ebe3fe421ad59f70f0490af87
Your Name <you@example.com>
2020-08-31 18:08:26 +0800

    实现阻尼效果

--- TestScripts/FreeCamera.cs
+++ TestScripts/FreeCamera.cs
@@ -13,10 +13,15 @@ public class FreeCamera : MonoBehaviour
     public float speed=1.0f;
     [Range(0,5)]
     public float distance=2.0f; //distance from camera to the target
+   
+    [Range(0,5)]
+    public float rotDamp=2.0f;
+    [Range(0,50)]
+    public float rotSpeed=32.0f;
     public Transform target;
 
     public Vector3 pos;
-    private Quaternion rot;
+    private Quaternion rot,targetRot;
 
     public InputAxis X;
     public InputAxis Y;
@@ -27,9 +32,26 @@ public class FreeCamera : MonoBehaviour
     }
 
     void Update(){
+        ProcessInput();
+
+
+        //update rot
+        rot = Quaternion.Slerp(rot,targetRot,Time.deltaTime*rotDamp);
+        transform.rotation=rot;
+
+        // transform.rotation = Quaternion.Slerp(transform.rotation,targetRot,Time.deltaTime*rotDamp);
+
+        //update position
+        transform.position= pos -rot* new Vector3(0,0,distance);
+
+    }
+
+    void ProcessInput(){
         float dx= Input.GetAxis(X.ToString());
         float dy= Input.GetAxis(Y.ToString());
-
+        
+        dx*=rotSpeed;
+        dy*=rotSpeed;
         if (Input.GetMouseButton(1))
         {
             Vector3 angles= transform.rotation.eulerAngles;
@@ -38,11 +60,11 @@ public class FreeCamera : MonoBehaviour
             angles.y+=dx; //这里x轴Y轴的旋转 正好需要和XY反过来
             angles.x+=-dy;//这里方向也需要反一下
             angles.z=0;
-            rot.eulerAngles= angles;
-            transform.rotation=rot;
+            targetRot.eulerAngles= angles;
+            // transform.rotation=rot;
 
-            //update position
-            transform.position= pos -rot* new Vector3(0,0,distance);
+            // //update position
+            // transform.position= pos -rot* new Vector3(0,0,distance);
 
             //update position method 2
             // transform.position = pos - transform.forward * distance;
