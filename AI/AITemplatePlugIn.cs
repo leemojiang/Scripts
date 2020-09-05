f3c980c75f1cb506c2a8516f64398a5078601332
Your Name <you@example.com>
2020-09-05 20:20:54 +0800

    开始实现移动部分的AI

--- AI/AITemplatePlugIn.cs
+++ AI/AITemplatePlugIn.cs
@@ -2,7 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class AITemplatePlugIn{
+public abstract class AITemplatePlugIn:MonoBehaviour{
 
+    
+}
+
+public class ControlInfo:AITemplatePlugIn{
+
+}
+
+public class Mobile:AITemplatePlugIn{
     
 }
